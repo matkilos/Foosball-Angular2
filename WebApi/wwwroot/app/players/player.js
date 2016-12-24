81249ca2a8fa36dfc2c5d9211abd5dcbb827d923
Mateusz Kilos <matkilos@gmail.com>
2016-12-24 01:40:55 +0100

    Remove WebApi/wwwroot content and add it to .gitignore.

--- WebApi/wwwroot/app/players/player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Player = (function () {
-    function Player(init) {
-        Object.assign(this, init);
-    }
-    return Player;
-}());
-exports.Player = Player;
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBsYXllcnMvcGxheWVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTtJQUlJLGdCQUFZLElBQXNCO1FBQzlCLE1BQU0sQ0FBQyxNQUFNLENBQUMsSUFBSSxFQUFFLElBQUksQ0FBQyxDQUFDO0lBQzlCLENBQUM7SUFDTCxhQUFDO0FBQUQsQ0FQQSxBQU9DLElBQUE7QUFQWSx3QkFBTSIsImZpbGUiOiJwbGF5ZXJzL3BsYXllci5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBQbGF5ZXIge1xyXG4gICAgcHVibGljIGlkOiBzdHJpbmc7XHJcbiAgICBwdWJsaWMgbmFtZTogc3RyaW5nO1xyXG5cclxuICAgIGNvbnN0cnVjdG9yKGluaXQ/OiBQYXJ0aWFsPFBsYXllcj4pIHtcclxuICAgICAgICBPYmplY3QuYXNzaWduKHRoaXMsIGluaXQpO1xyXG4gICAgfVxyXG59Il19
